78b63461cdf836597129c205d774650a2c833974
Тетерин Святослав <svyatoslavteterin@gmail.com>
2017-12-03 22:16:32 +0300

    fix bug resulsved

--- resources/views/emails/saved/result.blade.php
+++ resources/views/emails/saved/result.blade.php
@@ -1,4 +1,4 @@
-<h2>{{$session->base}}</h2>
+<h2>{{$session['base']}}</h2>
 <div class="ring-info__desc"> </div>
 
 <h3>Оправа</h3>
@@ -6,29 +6,29 @@
 <table>
   <tr>
     <th>Материал<th>
-    <td>{{$session->material}}<td>
+    <td>{{$session['base']}}<td>
   </tr>
 
 </table>
 
-<h3>{{$session->stone}}</h3>
+<h3>{{$session['base']}}</h3>
 
 <table>
   <tr>
     <th>Форма огранки<th>
-    <td>{{$session->shape}}<td>
+    <td>{{$session['base']}}<td>
   </tr>
   <tr>
     <th>Вес камня<th>
-    <td>{{$session->weight}}<td>
+    <td>{{$session['base']}}<td>
   </tr>
   <tr>
     <th>Качество<th>
-    <td>{{$session->color}}<td>
+    <td>{{$session['base']}}<td>
   </tr>
   <tr>
     <th>Диаметр<th>
-    <td>{{$session->size}}<td>
+    <td>{{$session['base']}}<td>
   </tr>
 
 </table>
