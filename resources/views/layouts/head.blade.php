03ac0d8a8af16c3baa5f46aa9fccfe2d25660a67
Тетерин Святослав <svyatoslavteterin@gmail.com>
2017-12-05 09:29:20 +0300

    add favicon

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -2,6 +2,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<link rel="icon" type="image/png" href="/favicon32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon16.png" sizes="16x16">
 
 <title>Briliant Rings @if (isset($pagetitle)) | {{$pagetitle}} @endif</title>
 
