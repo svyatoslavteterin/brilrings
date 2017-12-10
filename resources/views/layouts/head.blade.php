7959f53e27f3ae69c3ba13dafeca85240912e75c
Тетерин Святослав <svyatoslavteterin@gmail.com>
2017-12-10 14:55:09 +0300

    remove iphone scale forms

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1,minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="icon" type="image/png" href="/favicon32.png" sizes="32x32">
 <link rel="icon" type="image/png" href="/favicon16.png" sizes="16x16">
